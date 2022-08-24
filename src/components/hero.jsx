1705da66f2c2caad49fa04f8364706c30f99e57b
JP <j.p.walter@outlook.com>
2022-08-24 16:55:21 -0400

    Commit before first deploy

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -12,6 +12,7 @@ function Hero() {
           Join unique interactive activities led by one-of-a-kind hosts--all
           without leaving home.
         </p>
+        <p>Pictures are broken :(</p>
       </main>
     </section>
   );
